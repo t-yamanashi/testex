76ddefa0676947aa7d4c97cc8639fd27fc0167eb
Tomoyuki Yamanashi <ae86bg5@yahoo.co.jp>
2021-06-10 08:06:24 +0900

    Update execdocker.sh

--- execdocker.sh
+++ execdocker.sh
@@ -3,4 +3,4 @@
 chmod 755 phx.sh
 sudo docker build  -t ex .
 #sudo docker run -it -p 4000:4000 -v `pwd`:/root -w /root ex
-sudo docker run -it -p 4000:4000 -v `pwd`/hoge:/root/hoge -w /root/hoge ex
+sudo docker run -it -p 4000:4000 -v `pwd`/hoge:/root/hoge ex
