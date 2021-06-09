42ab0111a6e03ee4d270a80eabaa773f2d111591
Tomoyuki Yamanashi <ae86bg5@yahoo.co.jp>
2021-06-10 07:45:33 +0900

    Update phx.sh

--- phx.sh
+++ phx.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-yes | mix archive.install hex phx_new 1.4.17
+#yes | mix archive.install hex phx_new 1.4.17
 yes | mix phx.new hoge --no-ecto --no-webpack
 cd hoge
 echo y | mix phx.server
